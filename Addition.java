2b99cbbd2cf593b364df3e6251d546ea1d38d0b6
AnbuSivaji <anbusoft57@gmail.com>
2025-03-12 16:44:22 +0530

    Addition

---
+++ Addition.java
@@ -0,0 +1,13 @@
+
+
+class Addition {
+    void add(){
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of a and b is: "+(a+b));
+    }
+    public static void main(String args[]){
+        Addition obj = new Addition();
+        obj.add();
+    }
+}
---
+++ Addition1.java
@@ -0,0 +1,10 @@
+ class Addition1 {
+    static void add(int a, int b){
+        int sum=a+b;
+        System.out.println("The sum of a and b is: "+sum);
+    }
+    public static void main(String args[]){
+        add(100,200);
+    }
+    
+}
